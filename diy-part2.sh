e2a8b7b364603518dba9da824c2de685727bb347
10004909 <122661265+10004909@users.noreply.github.com>
2023-05-10 08:18:49 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -20,6 +20,4 @@ find ./ -name luci-app-argon-config | xargs rm -rf;
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
 
-rm -rf feeds/small8/luci-app-adguardhome
 
-git clone https://github.com/10004909/luci-app-adguardhome.git package/net/luci-app-adguardhome
