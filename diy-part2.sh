cd4576b22159489b5f633c347d7336541a3bd084
10004909 <122661265+10004909@users.noreply.github.com>
2023-05-25 08:35:44 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -21,3 +21,6 @@ git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lea
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
 
 
+rm -rf feeds/small8/luci-app-adguardhome
+
+git clone https://github.com/10004909/luci-app-adguardhome.git package/net/luci-app-adguardhome
